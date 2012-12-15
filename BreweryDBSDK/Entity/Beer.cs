efecfa566036973fb1309ab48a26eaa4791c80aa
Tyler Walborn <im.t.wal@gmail.com>
2012-12-15 16:24:23 -0700

    Added available

--- BreweryDBSDK/Entity/Beer.cs
+++ BreweryDBSDK/Entity/Beer.cs
@@ -12,11 +12,13 @@ namespace BreweryDBSDK.Entity
         public string name { get; set; }
         public long styleId { get; set; }
         public long glasswareId { get; set; }
+        public long availableId { get; set; }
         public string isOrganic { get; set; }
         public string status { get; set; }
         public DateTime createDate { get; set; }
         public DateTime updateDate { get; set; }
         public Glass glass { get; set; }
         public Style style { get; set; }
+        public Available available { get; set; }
     }
 }
